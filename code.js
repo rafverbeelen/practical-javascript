75f2dcea9a503354564c06b51eb4aaa73a440d8d
rafverbeelen <raf.verbeelen@icloud.com>
2018-10-15 19:58:36 +0100

    Version 4 - Booleans

--- code.js
+++ code.js
@@ -1,23 +1,32 @@
 // It should store the todos array on an object
 var todoList = {
-  todos: ['item 1', 'item 2', 'item 3'],
+  todos: [],
   // It should have a displayTodos method
   displayTodos: function() {
     console.log('My Todos:', this.todos);
   },
-  // It should have an addTodo method
-  addTodo: function(todo) {
-    this.todos.push(todo);
+  // todoList.addTodo should add objects
+  addTodo: function(todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false;
+    });
     this.displayTodos();
   },
-  // It should have an changeTodo method
-  changeTodo: function(position, newValue) {
-    this.todos[position] = newValue;
+  // todoList.changeTodo should change the todoText property
+  changeTodo: function(position, todoText) {
+    this.todos[position].todoText = todoText;
     this.displayTodos();
   },
   // It should have an deleteTodo method
   deleteTodo: function(position) {
     this.todos.splice(position, 1);
+    this.displayTodos();
+  },
+  // todoList.toggleCompleted should change the completed property
+  toggleCompleted: function(position) {
+    var todo = this.todos[position];
+    todo.completed = !todo.completed;
     this.displayTodos();
   }
 };
