07387e06f50a54af62dd1fd6f5128cfe3c991ba2
rafverbeelen <raf.verbeelen@icloud.com>
2018-10-15 19:44:35 +0100

    Version 3 - Objects

--- code.js
+++ code.js
@@ -1,25 +1,23 @@
-// It should have a place to store todos
-var todos = ['item 1', 'item 2', 'item 3'];
-
-// It should have a function to display todos
-function displayTodos() {
-  console.log('My Todos:', todos);
-}
-
-// It should have a function to add todos
-function addTodo(todo) {
-  todos.push(todo);
-  displayTodos();
-}
-
-// It should have a function to change todos
-function changeTodo(position, newValue) {
-  todos[position] = newValue;
-  displayTodos();
-}
-
-// It should have a function to delete todos
-function deleteTodo(position) {
-  todos.splice(position, 1);
-  displayTodos();
-}
+// It should store the todos array on an object
+var todoList = {
+  todos: ['item 1', 'item 2', 'item 3'],
+  // It should have a displayTodos method
+  displayTodos: function() {
+    console.log('My Todos:', this.todos);
+  },
+  // It should have an addTodo method
+  addTodo: function(todo) {
+    this.todos.push(todo);
+    this.displayTodos();
+  },
+  // It should have an changeTodo method
+  changeTodo: function(position, newValue) {
+    this.todos[position] = newValue;
+    this.displayTodos();
+  },
+  // It should have an deleteTodo method
+  deleteTodo: function(position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  }
+};
