be6ced4f46c8485b7363f43f61839b51de152fdf
rafverbeelen <raf.verbeelen@icloud.com>
2018-10-15 21:26:09 +0100

    Version 9 - Escape from the console

--- code.js
+++ code.js
@@ -1,46 +1,25 @@
 // It should store the todos array on an object
 var todoList = {
   todos: [],
-  // .displayTodos should show .todoText
-  // .displayTodos should tell if .todos is empty
-  // .displayTodos should show .completed
-  displayTodos: function() {
-    if (this.todos.length === 0) {
-      console.log('Your todo list is empty.')
-    } else {
-      console.log('My Todos:');
-      for (var i = 0; i < this.todos.length; i++) {
-        if (this.todos[i].completed === true) {
-          console.log('(x)', this.todos[i].todoText);
-        } else {
-          console.log('( )', this.todos[i].todoText);
-        }
-      }
-    }
-  },
   // todoList.addTodo should add objects
   addTodo: function(todoText) {
     this.todos.push({
       todoText: todoText,
       completed: false
     });
-    this.displayTodos();
   },
   // todoList.changeTodo should change the todoText property
   changeTodo: function(position, todoText) {
     this.todos[position].todoText = todoText;
-    this.displayTodos();
   },
   // It should have an deleteTodo method
   deleteTodo: function(position) {
     this.todos.splice(position, 1);
-    this.displayTodos();
   },
   // todoList.toggleCompleted should change the completed property
   toggleCompleted: function(position) {
     var todo = this.todos[position];
     todo.completed = !todo.completed;
-    this.displayTodos();
   },
   // .toggleAll: if everything’s true, make everything false
   // .toggleAll: otherwise, make everything true
@@ -61,19 +40,16 @@ var todoList = {
         this.todos[i].completed = true;
       }
     }
-    this.displayTodos();
   }
 };
 
 var handlers = {
-  displayTodos: function() {
-    todoList.displayTodos();
-  },
   // It should have working controls for .addTodo
   addTodo: function() {
     var addTodoTextInput = document.getElementById('addTodoTextInput');
     todoList.addTodo(addTodoTextInput.value);
     addTodoTextInput.value = '';
+    view.displayTodos();
   },
   // It should have working controls for .changeTodo
   changeTodo: function() {
@@ -82,20 +58,50 @@ var handlers = {
     todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
     changeTodoPositionInput.value = '';
     changeTodoTextInput.value = '';
+    view.displayTodos();
   },
   // It should have working controls for .deleteTodo
   deleteTodo: function() {
     var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
     todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber)
     deleteTodoPositionInput.value = '';
+    view.displayTodos();
   },
   // It should have working controls for .toggleCompleted
   toggleCompleted: function() {
     var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
     todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
     toggleCompletedPositionInput.value = '';
+    view.displayTodos();
   },
   toggleAll: function() {
     todoList.toggleAll();
+    view.displayTodos();
+  }
+};
+
+// There should be a li element for every todo
+// Each li element should contain .todoText
+// Each li element should show .completed
+var view = {
+  displayTodos: function() {
+
+    var todosUl = document.querySelector('ul');
+    todosUl.innerHTML = '';
+
+    for (var i = 0; i < todoList.todos.length; i++) {
+      var todoLi = document.createElement('li');
+      var todo = todoList.todos[i];
+      var todoTextWithCompletion = '';
+
+      if (todo.completed === true) {
+        todoTextWithCompletion = '(x) ' + todo.todoText;
+      } else {
+        todoTextWithCompletion = '( ) ' + todo.todoText;
+      }
+
+      todoLi.textContent = todoTextWithCompletion;
+      todosUl.appendChild(todoLi);
+    }
   }
 };
